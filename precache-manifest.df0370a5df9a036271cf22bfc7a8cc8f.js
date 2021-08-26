9feabdf8e7d85d37a988e4574c06bec9872889e7
mikaeljohnson <mikaeljohnsoncrypto@gmail.com>
2021-08-26 13:15:41 -0400

    Updates

---
+++ precache-manifest.df0370a5df9a036271cf22bfc7a8cc8f.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "020da1cb3c5b35448b3bb41fe8f4160f",
+    "url": "./index.html"
+  },
+  {
+    "revision": "300632c13759a9a7d53e",
+    "url": "./static/css/2.80dce9aa.chunk.css"
+  },
+  {
+    "revision": "3699db9878d9924d2d18",
+    "url": "./static/css/main.18de8aef.chunk.css"
+  },
+  {
+    "revision": "300632c13759a9a7d53e",
+    "url": "./static/js/2.a8ae93fe.chunk.js"
+  },
+  {
+    "revision": "30545a24812bc2557c4d652dede4cf2a",
+    "url": "./static/js/2.a8ae93fe.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3699db9878d9924d2d18",
+    "url": "./static/js/main.3e57799b.chunk.js"
+  },
+  {
+    "revision": "35eff47ceeca097ad7eb",
+    "url": "./static/js/runtime-main.2bd40bad.js"
+  },
+  {
+    "revision": "d6cbd8e6d17d679dd3cd7fe3884ac8bb",
+    "url": "./static/media/photo.d6cbd8e6.png"
+  }
+]);
